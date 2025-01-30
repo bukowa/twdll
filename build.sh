cf1282791f22c1708c9195a2fd30e23530ba6190
Bukowa <gitbukowa@gmail.com>
2025-01-30 12:51:20 +0100

    chore: add rungame

--- build.sh
+++ build.sh
@@ -6,3 +6,4 @@ export CXX="ccache g++"
 make -C ./lua-5.1.5 MYCFLAGS="-m32" MYLDFLAGS="-m32" mingw
 cmake --build . --target twdll -j 22
 cp libtwdll.dll "C:\Program Files (x86)\Steam\steamapps\common\Total War Rome II"
+start "" rungame.lnk
